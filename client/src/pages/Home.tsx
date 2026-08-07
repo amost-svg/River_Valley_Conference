2922c02a3876edf5f78912bff743ec3b6017977f
amost-svg <amost@gracecrusaders.org>
2026-08-07 14:58:17 -0500

    Move conference events off homepage into calendar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 import ConferenceOverview from "@/components/ConferenceOverview";
 import MemberSchools from "@/components/MemberSchools";
 import SchedulesResults from "@/components/SchedulesResults";
-import ConferenceEvents from "@/components/ConferenceEvents";
 import ConferenceStandings from "@/components/ConferenceStandings";
 import NewsAnnouncements from "@/components/NewsAnnouncements";
 import ContactSection from "@/components/ContactSection";
@@ -15,7 +14,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50">
       <Seo
         title="River Valley Conference | Illinois High School Athletics"
-        description="Official website of the River Valley Conference featuring 10 IHSA member schools, game schedules, conference standings, events, athletics news, and contact information."
+        description="Official website of the River Valley Conference featuring 10 IHSA member schools, game schedules, conference calendar, standings, athletics news, and contact information."
         type="website"
       />
       <Navigation />
@@ -23,7 +22,6 @@ export default function Home() {
       <ConferenceOverview />
       <MemberSchools />
       <SchedulesResults />
-      <ConferenceEvents />
       <ConferenceStandings />
       <NewsAnnouncements />
       <ContactSection />
